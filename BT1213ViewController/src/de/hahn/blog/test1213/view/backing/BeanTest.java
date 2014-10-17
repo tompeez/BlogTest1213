ad1b594d7c01ad066faf43af5b70532ff593c7a7
Timo Hahn <tompeez@gmail.com>
2014-10-17 10:17:27 +0200

    Changes

---
+++ BT1213ViewController/src/de/hahn/blog/test1213/view/backing/BeanTest.java
@@ -0,0 +1,93 @@
+package de.hahn.blog.test1213.view.backing;
+
+import oracle.adf.view.rich.component.rich.RichDocument;
+import oracle.adf.view.rich.component.rich.RichForm;
+import oracle.adf.view.rich.component.rich.input.RichInputText;
+import oracle.adf.view.rich.component.rich.layout.RichGridCell;
+import oracle.adf.view.rich.component.rich.layout.RichGridRow;
+import oracle.adf.view.rich.component.rich.layout.RichPanelGridLayout;
+import oracle.adf.view.rich.component.rich.output.RichOutputText;
+
+public class BeanTest {
+    private RichGridCell gc1;
+    private RichGridRow gr1;
+    private RichGridCell gc2;
+    private RichGridRow gr2;
+    private RichPanelGridLayout pgl1;
+    private RichForm f1;
+    private RichDocument d1;
+    private RichOutputText ot5;
+    private RichInputText it1;
+
+    public void setGc1(RichGridCell gc1) {
+        this.gc1 = gc1;
+    }
+
+    public RichGridCell getGc1() {
+        return gc1;
+    }
+
+    public void setGr1(RichGridRow gr1) {
+        this.gr1 = gr1;
+    }
+
+    public RichGridRow getGr1() {
+        return gr1;
+    }
+
+    public void setGc2(RichGridCell gc2) {
+        this.gc2 = gc2;
+    }
+
+    public RichGridCell getGc2() {
+        return gc2;
+    }
+
+    public void setGr2(RichGridRow gr2) {
+        this.gr2 = gr2;
+    }
+
+    public RichGridRow getGr2() {
+        return gr2;
+    }
+
+    public void setPgl1(RichPanelGridLayout pgl1) {
+        this.pgl1 = pgl1;
+    }
+
+    public RichPanelGridLayout getPgl1() {
+        return pgl1;
+    }
+
+    public void setF1(RichForm f1) {
+        this.f1 = f1;
+    }
+
+    public RichForm getF1() {
+        return f1;
+    }
+
+    public void setD1(RichDocument d1) {
+        this.d1 = d1;
+    }
+
+    public RichDocument getD1() {
+        return d1;
+    }
+
+    public void setOt5(RichOutputText ot5) {
+        this.ot5 = ot5;
+    }
+
+    public RichOutputText getOt5() {
+        return ot5;
+    }
+
+    public void setIt1(RichInputText it1) {
+        this.it1 = it1;
+    }
+
+    public RichInputText getIt1() {
+        return it1;
+    }
+}
