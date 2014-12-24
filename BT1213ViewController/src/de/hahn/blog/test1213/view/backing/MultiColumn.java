b73dddd0a2cb1d3e7afdf419b31fdecfd7f0bee9
Timo Hahn <tompeez@gmail.com>
2014-12-24 13:09:31 +0100

    Stuff added

---
+++ BT1213ViewController/src/de/hahn/blog/test1213/view/backing/MultiColumn.java
@@ -0,0 +1,183 @@
+package de.hahn.blog.test1213.view.backing;
+
+import oracle.adf.view.rich.component.rich.RichDocument;
+import oracle.adf.view.rich.component.rich.RichForm;
+import oracle.adf.view.rich.component.rich.RichQuery;
+import oracle.adf.view.rich.component.rich.data.RichTable;
+import oracle.adf.view.rich.component.rich.input.RichInputDate;
+import oracle.adf.view.rich.component.rich.layout.RichGridCell;
+import oracle.adf.view.rich.component.rich.layout.RichGridRow;
+import oracle.adf.view.rich.component.rich.layout.RichPanelGridLayout;
+import oracle.adf.view.rich.component.rich.layout.RichPanelGroupLayout;
+import oracle.adf.view.rich.component.rich.layout.RichPanelHeader;
+import oracle.adf.view.rich.component.rich.nav.RichLink;
+import oracle.adf.view.rich.component.rich.output.RichMessages;
+import oracle.adf.view.rich.component.rich.output.RichOutputText;
+import oracle.adf.view.rich.component.rich.output.RichPanelCollection;
+
+public class MultiColumn {
+    private RichGridCell gc2;
+    private RichGridRow gr1;
+    private RichGridCell gc1;
+    private RichGridRow gr2;
+    private RichPanelGridLayout pgl1;
+    private RichForm f1;
+    private RichDocument d1;
+    private RichOutputText ot1;
+    private RichMessages m1;
+    private RichOutputText ot28;
+    private RichPanelGroupLayout pgl2;
+    private RichPanelHeader ph1;
+    private RichQuery qryId1;
+    private RichTable resId1;
+    private RichInputDate id3;
+    private RichInputDate id4;
+    private RichPanelCollection pc1;
+    private RichLink l1;
+
+    public void setGc2(RichGridCell gc2) {
+        this.gc2 = gc2;
+    }
+
+    public RichGridCell getGc2() {
+        return gc2;
+    }
+
+    public void setGr1(RichGridRow gr1) {
+        this.gr1 = gr1;
+    }
+
+    public RichGridRow getGr1() {
+        return gr1;
+    }
+
+    public void setGc1(RichGridCell gc1) {
+        this.gc1 = gc1;
+    }
+
+    public RichGridCell getGc1() {
+        return gc1;
+    }
+
+    public void setGr2(RichGridRow gr2) {
+        this.gr2 = gr2;
+    }
+
+    public RichGridRow getGr2() {
+        return gr2;
+    }
+
+    public void setPgl1(RichPanelGridLayout pgl1) {
+        this.pgl1 = pgl1;
+    }
+
+    public RichPanelGridLayout getPgl1() {
+        return pgl1;
+    }
+
+    public void setF1(RichForm f1) {
+        this.f1 = f1;
+    }
+
+    public RichForm getF1() {
+        return f1;
+    }
+
+    public void setD1(RichDocument d1) {
+        this.d1 = d1;
+    }
+
+    public RichDocument getD1() {
+        return d1;
+    }
+
+    public void setOt1(RichOutputText ot1) {
+        this.ot1 = ot1;
+    }
+
+    public RichOutputText getOt1() {
+        return ot1;
+    }
+
+
+    public void setM1(RichMessages m1) {
+        this.m1 = m1;
+    }
+
+    public RichMessages getM1() {
+        return m1;
+    }
+
+
+    public void setOt28(RichOutputText ot28) {
+        this.ot28 = ot28;
+    }
+
+    public RichOutputText getOt28() {
+        return ot28;
+    }
+
+    public void setPgl2(RichPanelGroupLayout pgl2) {
+        this.pgl2 = pgl2;
+    }
+
+    public RichPanelGroupLayout getPgl2() {
+        return pgl2;
+    }
+
+    public void setPh1(RichPanelHeader ph1) {
+        this.ph1 = ph1;
+    }
+
+    public RichPanelHeader getPh1() {
+        return ph1;
+    }
+
+    public void setQryId1(RichQuery qryId1) {
+        this.qryId1 = qryId1;
+    }
+
+    public RichQuery getQryId1() {
+        return qryId1;
+    }
+
+    public void setResId1(RichTable resId1) {
+        this.resId1 = resId1;
+    }
+
+    public RichTable getResId1() {
+        return resId1;
+    }
+
+    public void setId3(RichInputDate id3) {
+        this.id3 = id3;
+    }
+
+    public RichInputDate getId3() {
+        return id3;
+    }
+
+    public void setId4(RichInputDate id4) {
+        this.id4 = id4;
+    }
+
+    public RichInputDate getId4() {
+        return id4;
+    }
+
+    public void setPc1(RichPanelCollection pc1) {
+        this.pc1 = pc1;
+    }
+
+    public RichPanelCollection getPc1() {
+        return pc1;
+    }
+
+    public void setL1(RichLink l1) {
+        this.l1 = l1;
+    }
+
+    public RichLink getL1() {
+        return l1;
+    }
+}
