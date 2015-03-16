36c531b28225be4e137886c325d5c5a6e7fa4913
Timo Hahn <tompeez@gmail.com>
2015-03-16 10:19:09 +0100

    misc

---
+++ BT1213ViewController/src/de/hahn/blog/test1213/view/backing/DialogBean.java
@@ -0,0 +1,12 @@
+package de.hahn.blog.test1213.view.backing;
+
+import org.apache.myfaces.trinidad.event.ReturnEvent;
+
+public class DialogBean {
+    public DialogBean() {
+    }
+
+    public void dialogReturnListener(ReturnEvent returnEvent) {
+        // Add event code here...
+    }
+}
